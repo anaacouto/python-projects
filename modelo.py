04eb8c243b683026d8708eba599b8e1fc32ac5d7
Ana Couto <ana.couto@armateus.com.br>
2021-05-03 11:37:08 -0300

    Herança mútipla e manipulação de strings.'

--- modelo.py
+++ modelo.py
@@ -1,28 +1,37 @@
-class Programa:
+from abc import ABCMeta, abstractmethod
+
+class Programa(metaclass = ABCMeta):
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__likes = 0
+        self._likes = 0
     
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome.title()
+        self._nome = nome.title()
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
     
     def dar_likes(self):
-        self.__likes += 1
+        self._likes += 1
+    
+    @abstractmethod
+    def __str__(self):
+        pass
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes'
 
 
 class Serie(Programa):
@@ -30,11 +39,38 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_likes()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes} likes'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+    
+    def __len__(self):
+        return len(self._programas)
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+demolidor = Serie('Demolidor', 2016, 2)
+tmep = Filme('Todo mundo em Pânico', 1999, 100)
 atlanta = Serie('atlanta', 2018, 2)
+
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+
+filmes_e_series = [vingadores, atlanta, tmep, demolidor]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
