44527d3e8e8b893fc9c2450b8acbe2f1a545bb59
Ana Couto <ana.couto@armateus.com.br>
2021-05-03 16:45:03 -0300

    Manipulando strings.

--- strings.py
+++ strings.py
@@ -1,8 +1,18 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
 
-url_base = url[0:19]
-print(url_base)
+# separando url base e parametros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 
-url_parametros = url[20:36]
-print(url_parametros)
+# pegando valor dos parametros
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
