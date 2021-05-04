a3f80a772bcc3dfefba99aa87e132b2a00af324e
Ana Couto <ana.couto@armateus.com.br>
2021-05-04 10:48:49 -0300

    Melhorando validação da url e implementando conversão de moedas.

--- extratorurl.py
+++ extratorurl.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorUrl:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -12,6 +14,10 @@ class ExtratorUrl:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+        padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
         indice_interrogacao = url.find('?')
@@ -31,10 +37,33 @@ class ExtratorUrl:
             valor = self.get_url_parametros()[indice_valor:]
         else:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
-
         return valor
+    
+    def converter(self):
+        dolar = 5.50
+        quantidade = self.get_valor_parametro("quantidade")
+        moedaOrigem = self.get_valor_parametro("moedaOrigem")
+        moedaDestino = self.get_valor_parametro("moedaDestino")
+        if moedaOrigem == 'real' and moedaDestino == 'dolar':
+            valor_conversao = int(quantidade) / dolar
+            return valor_conversao
+        elif moedaOrigem == 'dolar' and moedaDestino == 'real':
+            valor_conversao = int(quantidade) * dolar
+            return valor_conversao
+        else:
+            raise ValueError("Não é possível fazer a conversão dessas moedas.")
 
-url = "bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+    def __len__(self):
+        return len(self.url)
+    
+    def __str__(self):
+        return self.url + '\nParâmetros: ' + self.get_url_parametros() + '\nURL Base: ' + self.get_url_base()
+    
+    def __eq__(self, other):
+        return self.url == other
+
+url = "bytebank.com/cambio?quantidade=100&moedaDestino=real&moedaOrigem=dolar"
 extrator_url = ExtratorUrl(url)
-valor_quantidade = extrator_url.get_valor_parametro(input("Qual o parametro (quantidade, moedaDestino ou moedaOrigem) ? "))
-print(valor_quantidade)
+# valor_quantidade = extrator_url.get_valor_parametro(input("Qual o parametro (quantidade, moedaDestino ou moedaOrigem)? "))
+# print(valor_quantidade)
+print(extrator_url.converter())
